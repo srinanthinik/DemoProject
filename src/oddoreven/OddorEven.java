d518c966e0d7161548e1cdb07d70bf445303dd96
srinanthinik <srinanthinik@gmail.com>
2022-08-02 15:22:12 +0530

    demomail

---
+++ src/oddoreven/OddorEven.java
@@ -0,0 +1,34 @@
+package oddoreven;
+
+public class OddorEven {
+	
+	
+	 
+	public static void main(String args[])
+	{
+	int n = 10;
+	if(n%2 != 0)
+	{
+	System.out.println("Weird");
+	}
+	else
+	{
+	if(n >=2 && n <=5)
+	{
+	System.out.println("Not Weird");
+	}
+	else if(n >=6 && n<= 20)
+	{
+	System.out.println("Weird");
+	}
+	else if(n>20)
+	{
+	System.out.println("Not Weird");
+	}
+	}
+	}
+	}
+
+
+
+
